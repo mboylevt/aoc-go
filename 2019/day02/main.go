5b5465b004a434e113c6ac60f8fae21f678dd227
mattboyle <matt.boyle@datadoghq.com>
2023-11-09 07:31:27 +0100

    Day2 working

--- 2019/day02/main.go
+++ 2019/day02/main.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 
 	"github.com/mboylevt/aoc-go/cast"
+	"github.com/mboylevt/aoc-go/intcode"
 	"github.com/mboylevt/aoc-go/util"
 )
 
@@ -27,23 +28,35 @@ func main() {
 	flag.Parse()
 	fmt.Println("Running part", part)
 
-	if part == 1 {
-		ans := part1(input)
-		util.CopyToClipboard(fmt.Sprintf("%v", ans))
-		fmt.Println("Output:", ans)
-	} else {
-		ans := part2(input)
-		util.CopyToClipboard(fmt.Sprintf("%v", ans))
-		fmt.Println("Output:", ans)
-	}
+	// if part == 1 {
+	ans := part1(input)
+	util.CopyToClipboard(fmt.Sprintf("%v", ans))
+	fmt.Println("Output:", ans)
+	// } else {
+	ans2 := part2(input)
+	util.CopyToClipboard(fmt.Sprintf("%v", ans2))
+	fmt.Println("Output:", ans2)
+	// }
 }
 
 func part1(input string) int {
 	parsed := parseInput(input)
-	return util.IntCode(parsed)
+	return intcode.RunProgram(parsed)
 }
 
 func part2(input string) int {
+	for noun := 0; noun < 100; noun++ {
+		for verb := 0; verb < 100; verb++ {
+			parsed := parseInput(input)
+			parsed[1] = noun
+			parsed[2] = verb
+			var final_val = intcode.RunProgram(parsed)
+			fmt.Printf("N: %v V: %v Result: %v\n", noun, verb, final_val)
+			if final_val == 19690720 {
+				return 100*noun + verb
+			}
+		}
+	}
 	return 0
 }
 
