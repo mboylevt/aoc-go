56d5b437200f63e84fb2eb5d3159ea188646cce7
mattboyle <matt.boyle@datadoghq.com>
2023-12-07 20:29:49 -0500

    Day7P1

--- 2023/day07/main.go
+++ 2023/day07/main.go
@@ -4,8 +4,10 @@ import (
 	_ "embed"
 	"flag"
 	"fmt"
+	"sort"
 	"strings"
 
+	"github.com/mboylevt/aoc-go/cast"
 	"github.com/mboylevt/aoc-go/util"
 )
 
@@ -34,11 +36,113 @@ func main() {
 
 }
 
+type (
+	hand struct {
+		cards string
+		bid   int
+		ht    handType
+	}
+
+	handType int
+
+	typeCompute func(string) handType
+)
+
+var values = map[byte]int{
+	'2': 0,
+	'3': 1,
+	'4': 2,
+	'5': 3,
+	'6': 4,
+	'7': 5,
+	'8': 6,
+	'9': 7,
+	'T': 8,
+	'J': 9,
+	'Q': 10,
+	'K': 11,
+	'A': 12,
+}
+
+const (
+	highCard handType = iota
+	onePair
+	twoPair
+	threeKind
+	fullHouse
+	fourKind
+	fiveKind
+)
+
+func typeFromCards(in string) (out handType) {
+	sets := map[int][]byte{}
+
+	for len(in) > 0 {
+		card := in[0]
+		count := 0
+		for i := range in {
+			if in[i] == card {
+				count++
+			}
+		}
+		sets[count] = append(sets[count], card)
+		in = strings.ReplaceAll(in, in[0:1], "")
+	}
+
+	switch {
+	case sets[5] != nil:
+		out = fiveKind
+	case sets[4] != nil:
+		out = fourKind
+	case sets[3] != nil:
+		if sets[2] != nil {
+			out = fullHouse
+		} else {
+			out = threeKind
+		}
+	case sets[2] != nil:
+		if len(sets[2]) == 2 {
+			out = twoPair
+		} else {
+			out = onePair
+		}
+	default:
+		out = highCard
+	}
+	return out
+}
+
+func (h *hand) less(other hand, valResolutions map[byte]int) bool {
+	if h.ht == other.ht {
+		for i := range h.cards {
+			if h.cards[i] != other.cards[i] {
+				return valResolutions[h.cards[i]] < valResolutions[other.cards[i]]
+			}
+		}
+		return true
+	}
+	return h.ht < other.ht
+}
+
 func part1(input string) int {
 	parsed := parseInput(input)
 	_ = parsed
-
-	return 0
+	var hands []hand
+	for _, line := range parsed {
+		fields := strings.Fields(line)
+		cards := fields[0]
+		bid := cast.ToInt(fields[1])
+		hands = append(hands, hand{cards, bid, typeFromCards(cards)})
+	}
+	sort.Slice(hands, func(i, j int) bool {
+		return hands[i].less(hands[j], values)
+	})
+	fmt.Println("hi")
+	winnings := 0
+	for i, h := range hands {
+		winnings += (i + 1) * h.bid
+	}
+	return winnings
 }
 
 func part2(input string) int {
---
+++ 2023/day07/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test_part1(t *testing.T) {
+	input := `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`
+
+	want := 6440
+
+	if got := part1(input); got != want {
+		t.Errorf("part1() = %v, want %v", got, want)
+	}
+}
