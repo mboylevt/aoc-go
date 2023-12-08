ccfaaa291636399063d39deb6a8b430baa373946
mattboyle <matt.boyle@datadoghq.com>
2023-12-07 19:31:30 -0500

    Staging day 6/7

--- 2023/day06/main.go
+++ 2023/day06/main.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/mboylevt/aoc-go/cast"
 	"github.com/mboylevt/aoc-go/util"
@@ -22,13 +23,29 @@ func init() {
 
 func main() {
 	fmt.Println("Running part 1")
+	start := time.Now()
 	ans := part1(input)
+	elapsed := time.Since(start)
 	util.CopyToClipboard(fmt.Sprintf("%v", ans))
 	fmt.Println("Output:", ans)
-	fmt.Println("Running part 2")
+	fmt.Println("Part1 Time: ", elapsed)
+
+	fmt.Println("Running part dumb")
+	start = time.Now()
 	ans = part2(input)
+	elapsed = time.Since(start)
 	util.CopyToClipboard(fmt.Sprintf("%v", ans))
 	fmt.Println("Output:", ans)
+	fmt.Println("Part Dumb Time: ", elapsed)
+
+	fmt.Println("Running part 2")
+	start = time.Now()
+	ans = part2(input)
+	elapsed = time.Since(start)
+	util.CopyToClipboard(fmt.Sprintf("%v", ans))
+	fmt.Println("Output:", ans)
+	fmt.Println("Part2 Time: ", elapsed)
+
 }
 
 func getRecordBreaks(time int, record int) int {
@@ -71,6 +88,23 @@ func part2(input string) int {
 	time := cast.ToInt(strings.Join(strings.Fields(strings.Split(parsed[0], ":")[1]), ""))
 	record := cast.ToInt(strings.Join(strings.Fields(strings.Split(parsed[1], ":")[1]), ""))
 	return getRecordBreaks(time, record)
+}
+
+func partDumb(input string) int {
+	parsed := parseInput(input)
+	_ = parsed
+	time := cast.ToInt(strings.Join(strings.Fields(strings.Split(parsed[0], ":")[1]), ""))
+	record := cast.ToInt(strings.Join(strings.Fields(strings.Split(parsed[1], ":")[1]), ""))
+	result := record + 1
+	breaks := 0
+	// do it st00p
+	for i := 0; i < time; i++ {
+		result = (time - i) * (time - (time - i))
+		if result > record {
+			breaks++
+		}
+	}
+	return breaks
 }
 
 func parseInput(input string) (ans []string) {
