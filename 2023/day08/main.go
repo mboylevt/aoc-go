b02a3d0a69c08b7816073bd9bbb04d41c4af1aa1
mattboyle <matt.boyle@datadoghq.com>
2023-12-08 13:43:04 -0500

    Clean up + add lib functions

--- 2023/day08/main.go
+++ 2023/day08/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"container/ring"
 	_ "embed"
 	"flag"
 	"fmt"
 	"strings"
 
+	"github.com/mboylevt/aoc-go/lib"
 	"github.com/mboylevt/aoc-go/util"
 )
 
 //go:embed input.txt
 var input string
+
+const (
+	left int = iota
+	right
+)
 
 func init() {
 	// do this in init (not main) so test file has same input
@@ -34,15 +41,99 @@ func main() {
 
 }
 
+type node struct {
+	id    string
+	left  string
+	right string
+}
+
+func populateRingBuffer(input string) *ring.Ring {
+	r := ring.New(len(input))
+	for _, c := range input {
+		if c == 'L' {
+			r.Value = left
+		} else {
+			r.Value = right
+		}
+		r = r.Next()
+	}
+	return r
+}
+
+func populateNodes(nodeStrings []string) map[string]node {
+	nodeMap := make(map[string]node)
+
+	for _, str := range nodeStrings {
+		nodeFields := strings.Fields(str)
+		nodeId := nodeFields[0]
+		left := nodeFields[2][1:4]
+		right := nodeFields[3][0:3]
+		nodeMap[nodeId] = node{nodeId, left, right}
+	}
+
+	return nodeMap
+}
+
+func getNextNode(r *ring.Ring, n node, nodeMap map[string]node) node {
+	switch r.Value {
+	case left:
+		return nodeMap[n.left]
+	case right:
+		return nodeMap[n.right]
+	}
+	return n //this should never happen
+}
+
+func getStartingNodes(nodeMap map[string]node) []node {
+	var startingNodes []node
+	for key, n := range nodeMap {
+		if key[2] == 'A' {
+			startingNodes = append(startingNodes, n)
+		}
+	}
+	return startingNodes
+}
+
 func part1(input string) int {
 	parsed := parseInput(input)
-	_ = parsed
-
-	return 0
+	r := populateRingBuffer(parsed[0])
+	nodeMap := populateNodes(parsed[2:])
+	thisNode := nodeMap["AAA"]
+	stepCount := 0
+	for {
+		if thisNode.id == "ZZZ" {
+			break
+		}
+		thisNode = getNextNode(r, thisNode, nodeMap)
+		stepCount++
+		r = r.Next()
+	}
+	return stepCount
 }
 
 func part2(input string) int {
-	return 0
+	parsed := parseInput(input)
+	nodeMap := populateNodes(parsed[2:])
+	nodesToCheck := getStartingNodes(nodeMap)
+	fmt.Printf("Checking %v nodes\n", len(nodesToCheck))
+	var distances []int
+	// Check each node to find their steps to Z
+	for _, n := range nodesToCheck {
+		nodeStepCount := 0
+		r := populateRingBuffer(parsed[0])
+		for {
+			if n.id[2] == 'Z' {
+				break
+			}
+			n = getNextNode(r, n, nodeMap)
+			r = r.Next()
+			nodeStepCount++
+		}
+		distances = append(distances, nodeStepCount)
+	}
+
+	// run LCM checks on all nodes
+	return lib.LCM(distances[0], distances[1], distances[2:]...)
 }
 
 func parseInput(input string) (ans []string) {
--- 2023/day08/main_test.go
+++ 2023/day08/main_test.go
@@ -4,14 +4,32 @@ import (
 	"testing"
 )
 
-func Test_part1(t *testing.T) {
-	input := `32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-QQQJA 483`
+func Test_part1_first(t *testing.T) {
+	input := `RL
 
-	want := 6440
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)`
+
+	want := 2
+
+	if got := part1(input); got != want {
+		t.Errorf("part1() = %v, want %v", got, want)
+	}
+}
+
+func Test_part1_second(t *testing.T) {
+	input := `LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)`
+
+	want := 6
 
 	if got := part1(input); got != want {
 		t.Errorf("part1() = %v, want %v", got, want)
@@ -19,13 +37,18 @@ QQQJA 483`
 }
 
 func Test_part2(t *testing.T) {
-	input := `32T3K 765
-T55J5 684
-KK677 28
-KTJJT 220
-QQQJA 483`
+	input := `LR
 
-	want := 5905
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`
+
+	want := 6
 
 	if got := part2(input); got != want {
 		t.Errorf("part2() = %v, want %v", got, want)
